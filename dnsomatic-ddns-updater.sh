50b5272fd0c6fd482a529b907ff1bd74e92b0575
ethanpeterson@gmail.com <ethan@kaden.khp>
2021-10-13 09:30:13 -0500

    removing bash syntatical sugar to allow both sh & bash shells to run

--- dnsomatic-ddns-updater.sh
+++ dnsomatic-ddns-updater.sh
@@ -13,7 +13,8 @@ record_name=""                          # Which DNS record you want to be synced
 ###########################################
 ip=$(curl -s http://myip.dnsomatic.com/)
 
-if [ "${ip}" == "" ]; then 
+if [ "${ip}" == "" ]
+then 
   echo "DDNS Updater: No public IP found"
   exit 1
 fi
@@ -22,18 +23,28 @@ fi
 ## Get existing IP
 ###########################################
 # run this if we have a domain/record name to check against
-if [[ ! -z $record_name ]]; then
+if [ ! -z "$record_name" ]
+then
   # check if the dig command exists, if not fall back on nslookup
   digExists=$(command -v dig)
-  [[ ! -z $digExists ]] && old_ip=$(dig $record_name +short) || old_ip=$(nslookup $record_name | grep -o -E '([0-9][0-9]?[0-9]?\.?){4}$' | awk 'NR==2')
+
+  # if the dig command exists then use it to find our old ip address, otherwise fallback on nslookup
+  if [ ! -z "$digExists" ]
+  then
+    old_ip=$(dig $record_name +short)
+  else
+    old_ip=$(nslookup $record_name | grep -o -E '([0-9][0-9]?[0-9]?\.?){4}$' | awk 'NR==2')
+  fi
 
   # get today's date
   day=$(date +%d)
   # Check if today is the 14th or 28th
-  if [[ $day == 14 || $day == 28 ]]; then
+  if [ "$day" == 14 -o "$day" == 28 ]
+  then
     echo "We update DNS-O-Matic today to let them know we're still here."
   # Compare if they're the same
-  elif [[ $ip == $old_ip ]]; then
+  elif [ "${ip}" == "$old_ip" ]
+  then
     echo "DDNS Updater: IP ($ip) for ${record_name} has not changed."
     exit 0
   fi
@@ -42,8 +53,9 @@ fi
 ###########################################
 ## Change the IP@DNS-O-Matic using the API
 ###########################################
-if [ "${old_ip}" == "" ]; then 
-  echo "Requesting to update all.dnsomatic.com"
+if [ "${old_ip}" == "" ]
+then 
+  echo "Requesting to update all DNS records at DNS-O-Matic"
   update=$(curl -s --user-agent "Custom DDNS Updater - 0.1" -u $username:$password "https://updates.dnsomatic.com/nic/update?myip=$ip")
 else
   echo "Requesting to update a specific domain, so we're running update.dnsomatic.com"
